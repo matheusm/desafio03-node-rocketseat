36ca76e6971c91d18ce198a1235bca44345f813c
Matheus Martins <matheusms280100@gmail.com>
2023-09-26 16:27:45 -0300

    📍 Init project

---
+++ example.tsx
@@ -0,0 +1,8 @@
+
+export function Example() {
+  return (
+    <div>
+      <h1>Example</h1>
+    </div>
+  )
+}
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import { app } from "./app";
+
+app.listen({
+  host: '0.0.0.0',
+  port: 3000
+}).then(() => {
+  console.log('🚀 Server listening on http://localhost:3000')
+});
